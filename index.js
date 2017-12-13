6fdfb5d63bacf3698345bb4e32b5521297095cbc
William Spangler <bspangs@hotmail.com>
2017-12-13 16:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = "I don't do much.";
 
 kittens = ["Milo", "Otis", "Garfield"]; //global, is this way to reset?
 
-function destructivelyAppendKitten
+function destructivelyAppendKitten()
 
 
 /*removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged*/
-removeLastKitten()
+function removeLastKitten()
